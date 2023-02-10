73ebad22ef8599ad902d74bcfaac11700c0f79ff
blendi <dronramone@mail.ru>
2023-02-10 17:12:41 +0600

    Add new funktion in funcs.py

--- funcs.py
+++ funcs.py
@@ -6,3 +6,13 @@ def addition(a, b):
     :return:
     """
     return a + b
+
+
+def subtraction(a, b):
+    """
+    subtraction  two numbers
+    :param a:
+    :param b:
+    :return:
+    """
+    return a - b
